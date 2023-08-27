974c23a4fcae216b4a63f3aaef16ae5cc78c9fc0
kimewnseong <rladbstjd96@naver.com>
2023-08-27 17:27:13 +0900

    Rename

--- 효율성(투포인터, 슬라이딩윈도우, 해쉬)/두 배열 합치기.js
+++ 효율성(투포인터, 슬라이딩윈도우, 해쉬)/두 배열 합치기.js
@@ -2,7 +2,7 @@ function solution(arr1, arr2) {
   let answer = [];
   let n = arr1.length;
   let m = arr2.length;
-  let p1 = (p2 = 0);
+  let p1 = p2 = 0;
 
   while (p1 < n && p2 < m) {
     if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
